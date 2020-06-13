a76985db9787ad65f4ab5f330d1f1ef85cf5c2fe
johnnylin110 <dominatorjohnny@gmail.com>
2020-06-13 21:21:53 +0800

    update optlayer

--- ddpg_update.py
+++ ddpg_update.py
@@ -1,47 +1,35 @@
-
-"""
-Deep Deterministic Policy Gradient (DDPG), Reinforcement Learning.
-DDPG is Actor Critic based algorithm.
-Pendulum example.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-Using:
-tensorflow 1.0
-gym 0.8.0
-"""
-
-import tensorflow as tf
-import numpy as np
-import gym
-import time
-
 import os.path
 import sys
 
-import gym_BSS
+import gym
+import numpy as np
+import tensorflow as tf
+import gym_BSS  # noqa: F401
 
+name = sys.argv[1] if len(sys.argv) > 1 else 'BSSEnvTest-v0'
+env = gym.make(name)  # gym.Env
+env.seed(42)
+# print(env.observation_space, env.action_space)
+print(name)
+print(env.metadata)
 
 #####################  hyper parameters  ####################
-
-MAX_EPISODES = 200
-MAX_EP_STEPS = 200
-LR_A = 0.001    # learning rate for actor
-LR_C = 0.002    # learning rate for critic
-GAMMA = 0.9     # reward discount
-TAU = 0.01      # soft replacement
+LR_A = 0.001
+LR_C = 0.002 
+GAMMA = 0.9 
+TAU = 0.01
 MEMORY_CAPACITY = 10000
 BATCH_SIZE = 32
-
-RENDER = False
-# ENV_NAME = 'Pendulum-v0'
-ENV_NAME = 'BSSEnvTest-v0'
-
 #####################  BSS data functions  ####################
-
 def get_supriyo_policy_action(env, obs, policy):
     ypcmu, yncmu = policy
     env = env.unwrapped
     current_alloc = obs[env.nzones:2 * env.nzones]
+    #print(current_alloc)
+    #print(sum(current_alloc))
+    
     current_time = int(obs[-1])
+    #print(current_alloc)
     yp_t = np.array(ypcmu[current_time])
     yn_t = np.array(yncmu[current_time])
     return current_alloc + yn_t - yp_t
@@ -52,17 +40,17 @@ def read_supriyo_policy_results(env):
     scenario = env._scenario
     ypcmu = [[0.0 for k in range(env.nzones)] for j in range(env.ntimesteps)]
     yncmu = [[0.0 for k in range(env.nzones)] for j in range(env.ntimesteps)]
-    f1 = open(
-        os.path.join(env.data_dir, "Our_policy",
-                     "policy_result{0}.csv".format(scenario)))
+    f1 = open(os.path.join(env.data_dir, "Our_policy",
+                           "policy_result{0}.csv".format(scenario)))
     line = f1.readline()
-    line = f1.readline()
-    while (line != ""):
+    #line = f1.readline()
+    print(scenario)
+    while(line != ""):
         line = line.strip(" \n")
         line = line.split(",")
-        if (int(line[0]) < 100):
-            ypcmu[int(line[0]) + 1][int(line[1])] = float(line[2])
-            yncmu[int(line[0]) + 1][int(line[1])] = float(line[3])
+        if(int(line[0]) < 100):
+            ypcmu[int(line[0]) + 1][int(line[1])] = float(line[2])  #移出
+            yncmu[int(line[0]) + 1][int(line[1])] = float(line[3])  #移入
         line = f1.readline()
     f1.close()
     return (ypcmu, yncmu)
@@ -136,6 +124,8 @@ class DDPG(object):
         with tf.variable_scope(scope):
             net = tf.layers.dense(s, 30, activation=tf.nn.relu, name='l1', trainable=trainable)
             a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
+            print(a)
+            print(tf.multiply(a, self.a_bound, name='scaled_a'),"QQQ")
             return tf.multiply(a, self.a_bound, name='scaled_a')
 
     def _build_c(self, s, a, scope, trainable):
@@ -146,88 +136,151 @@ class DDPG(object):
             b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
             net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
             return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
+        
+################ Opt layer#####################
+            
+def OptLayer_function(action,a_dim,a_bound,env):
+    #adjust to y
+    maxa=action[np.argmax(action)]
+    mina=action[np.argmin(action)]
+    lower=np.zeros(a_dim)
+    y=np.zeros(a_dim)
+    print(env.nbikes,"bike_num")
+    
+    print(a_bound,"abound")
+    for i in range(a_dim):
+        y[i] = lower[i]+(a_bound[i]-lower[i])*(action[i]-mina)/(maxa-mina)
+    print(y,"y")
+    #adjust to z
+    z=np.zeros(a_dim)
+   
+    #start algorithm#
+    phase=0                          #  lower=0 , upeer=1 , done=2
+    C_unclamp=env.nbikes             # how many left bike to distribute
+    set_unclamp=set(range(a_dim))    # unclamp set
+    unclamp_num=a_dim                # unclamp number=n'
+    grad_z=np.zeros((a_dim,a_dim))   # grad_z is 4*4 arrray
+    while phase != 2 :
+        sum_y=0
+        set_clamp_round=set()  # indices clamped in this iteration of the while loop
+        #algorithm line 7
+        for i in range(a_dim):
+            if i in set_unclamp:
+                sum_y=sum_y+y[i]
+        for i in range(a_dim):
+            if i in set_unclamp:
+                z[i]=y[i]+(C_unclamp-sum_y)/unclamp_num
+        print(z,"z")
+        print(sum_y,"sum_y")
+        #algorithm line8
+        for i in range(a_dim):
+            if i in set_unclamp:
+                for j in range(a_dim):
+                    if j in set_unclamp:
+                        if (i!=j):
+                            grad_z[i][j]= -1/unclamp_num 
+                        else :
+                            grad_z[i][j]= 1- (1/unclamp_num)
+        print(grad_z)
+        #algorithm line 9
+        for j in range(a_dim):
+            if j not in set_unclamp :
+                for i in range(a_dim):
+                    grad_z[i][j]=0
+        print(grad_z,"grad before clamp in this iteration")
+        
+        #algorithm lin 10~20
+        for i in range(a_dim):
+            if i in set_unclamp:
+                if z[i]<lower[i] and phase==0 :
+                    z[i]=lower[i]
+                    for j in range(a_dim):
+                        grad_z[i][j]=0
+                    set_clamp_round.add(i)
+                elif (z[i]>a_bound[i]) and phase==1:
+                    z[i]=a_bound[i]
+                    for j in range(a_dim):
+                        grad_z[i][j]=0
+                    set_clamp_round.add(i)
+        print(z,"z_after clamp")
+        print(grad_z,"grad after clamp")
+        #algorithm 21~25
+        unclamp_num=unclamp_num-len(set_clamp_round)
+        print(unclamp_num,"unclamp")
+        for i in range(a_dim):
+            if i in set_clamp_round:
+                C_unclamp=C_unclamp-z[i]
+        print(C_unclamp,"C")
+        set_unclamp= set_unclamp.difference(set_clamp_round)
+        print(set_unclamp,"unclamp set")
+        if len(set_clamp_round)==0 :
+            phase=phase+1
+        
+    #debug after optlayer
+    final_sum=0
+    for i in range(a_dim):           
+        final_sum=final_sum+z[i]
+        assert lower[i]<=z[i]<=a_bound[i]   # make sure not violate the local constraint 
+    assert final_sum==env.nbikes     # make sure sum is equal to bike number
+    if np.sum(y)==env.nbikes:
+        assert z==y
 
-###############################  training  ####################################
-
-env = gym.make(ENV_NAME)
-env = env.unwrapped
-env.seed(42)
-# print(env.observation_space, env.action_space)
-print('ENV_NAME: ', ENV_NAME)
-print(env.metadata)
-
-s_dim = env.observation_space.shape[0]
+###############################  training  ####################################   
+Rs = []
+s_dim = env.observation_space.shape[0]  # 2*ZONE+1 , 前面ZONE個是Demand(這個ZONE被拿走幾台),後面ZONE個是number of resource on zone K (dS_) +time
+#又等於get_observe function in env
 a_dim = env.action_space.shape[0]
-a_bound = env.action_space.high
+#print(a_dim,"YEEEEEEE")
+#print(env.action_space.low,"low")
+a_bound = env.action_space.high   #最大上限,txt裡面設定的
 
 ddpg = DDPG(a_dim, s_dim, a_bound)
 
 var = 3  # control exploration
-t1 = time.time()
-for i in range(MAX_EPISODES):
-    s = env.reset()
-    # policy = read_supriyo_policy_results(env)
-    ep_reward = 0
-    ### info ###
+
+for ep in range(100):
+    R = 0
     ld_pickup = 0
     ld_dropoff = 0
     revenue = 0
     scenario = None
-    #############
-    for j in range(MAX_EP_STEPS):
-        if RENDER:
-            env.render()
+    done = False
+    s = env.reset()  # [0,0,0,0,8,7,8,8,0]
+    print(s)
+    #policy = read_supriyo_policy_results(env)
+    while not done:
+        #action = None
+        action=ddpg.choose_action(s)
+        print(action,"x")
+        OptLayer_function(action,a_dim,a_bound,env)
 
-        # Add exploration noise
-        a = None
-        a = ddpg.choose_action(s)
-        print('action random: ', a)
-        # a = get_supriyo_policy_action(env, s, policy)        
-        # a = np.clip(np.random.normal(a, var), 0.0, max(a_bound))    # add randomness to action selection for exploration
-        y = np.zeros(a.shape)
-        for i in range(a.shape[0]):
-            upper_bound = a_bound[i]
-            lower_bound = 0.0
-            y[i] = lower_bound + (upper_bound - lower_bound) * (a[i] - np.min(a)) / (np.max(a) - np.min(a))
-        print('y: ', y)
-
-        z = np.zeros(y.shape)
-        for i in range(y.shape[0]):
-            z[i] = y[i] + (env.nbikes - np.sum(y)) / (y.shape[0])
-        print('z: ', z)
+        # print(obs)
+        #action = get_supriyo_policy_action(env, obs, policy)
         
-        # for i in range(len(a)):
-        #     a[i] = np.clip(a[i], 0.0, a_bound[i])
-        # print('a_bound: ', a_bound)
-        # print('action clipped: ', a)
-        s_, r, done, info = env.step(z)
-
-        ddpg.store_transition(s, a, r / 10, s_)
-
+        #action = None
+        s_, r, done, info = env.step(action)
+        ddpg.store_transition(s, action, r / 10, s_)
         if ddpg.pointer > MEMORY_CAPACITY:
             var *= .9995    # decay the action randomness
             ddpg.learn()
-
         s = s_
-        ep_reward += r
-
-        ### info ###
+        R += r
         ld_pickup += info["lost_demand_pickup"]
         ld_dropoff += info["lost_demand_dropoff"]
         revenue += info["revenue"]
         scenario = info["scenario"]
-        ##############
+        
+    print({
+        'episode': ep,
+        'reward': R,
+        'Explore': var,
+        'lost_demand_pickup': ld_pickup,
+        "lost_demand_dropoff": ld_dropoff,
+        "revenue": revenue,
+        "scenario": scenario
+    })
+    Rs.append(R)
 
-        if j == MAX_EP_STEPS-1:
-            print({
-                'Episode': i, 
-                'Reward': int(ep_reward), 
-                'Explore': var, 
-                'lost_demand_pickup': ld_pickup,
-                "lost_demand_dropoff": ld_dropoff,
-                "revenue": revenue,
-                "scenario": scenario
-            })
-            # if ep_reward > -300:RENDER = True
-            break
-print('Running time: ', time.time() - t1)
+print('')
+print('---------------------------')
+print('Average reward per episode:', np.average(Rs))
