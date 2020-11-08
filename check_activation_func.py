c70f8e76a782ac422748a34714b3b72c09ab0fe3
upupsheep <sandy861003@gmail.com>
2020-11-09 00:47:04 +0800

    rewrite gradient

--- check_activation_func.py
+++ check_activation_func.py
@@ -10,13 +10,13 @@ import gym_BSS  # noqa: F401
 
 tf.compat.v1.disable_eager_execution()
 
+upper = np.array([3, 1, 5, 5], dtype=np.float32)
+lower = np.zeros(4, dtype=np.float32)
+
 ### Writing activation function ###
 
 
-def clipping_acti_func(x):
-    upper = np.array([3, 1, 5, 5], dtype=np.float32)
-    lower = np.zeros(4, dtype=np.float32)
-
+def clipping(x):
     y = np.zeros(4)
     print("bound: ", upper)
     print("acti input x: ", x)
@@ -31,42 +31,39 @@ def clipping_acti_func(x):
     return y
 
 
-np_clipping_acti_func = np.vectorize(
-    clipping_acti_func)  # vectorize the python function
+np_clipping = np.vectorize(clipping)  # vectorize the python function
+
 
 #############################################################################
 
 ### Gradient of Activation ###
 
 
-def d_clipping_acti_func(x):
-    upper = np.array([3, 1, 5, 5], dtype=np.float32)
-    lower = np.zeros(4, dtype=np.float32)
+def d_clipping(x):
+    max_i = np.argmax(x)
+    min_i = np.argmin(x)
 
-    y = np.zeros(4)
-    print("bound: ", upper)
-    print("acti input x: ", x)
-    # print(((x[0] - np.min(x)) / (np.max(x) - np.min(x))))
+    grad = np.zeros((4, 4))
     for i in range(4):
-        if x[i] <= upper[i] and x[i] >= lower[i]:
-            y[i] = x[i]
-        else:
-            y[i] = lower[i] + (upper[i] - lower[i]) * \
-                ((x[i] - np.min(x)) / (np.max(x) - np.min(x)))
-    print("acti output y:", y)
-    return y
+        if(i == max_i or i == min_i):
+            continue
+        # y[k] = upper[k] + (upper[k]-lower[k]) * {(x[i]-min(x))/(max(x)-min(x))}
+        grad[i][i] = (upper[i]-lower[i]) / (x[max_i] - x[min_i])
+    return grad
 
 
-np_d_clipping_acti_func = np.vectorize(d_clipping_acti_func)
+# np_d_clipping = np.vectorize(d_clipping) # don't need this one!
 
 
-def np_d_clipping_acti_func_32(x):
-    return np_d_clipping_acti_func(x).astype(np.float32)
+# def np_d_clipping_acti_func_32(x):
+#     return np_d_clipping_acti_func(x).astype(np.float32)
+def np_d_clipping_32(x):
+    return d_clipping(x).astype(np.float32)
 
 
-def tf_d_clipping_acti_func(x, name=None):
-    with tf.compat.v1.name_scope(name, "d_clipping_acti_func", [x]) as name:
-        y = py_func(np_d_clipping_acti_func_32,  # forward pass function
+def tf_d_clipping(x, name=None):
+    with tf.compat.v1.name_scope(name, "d_clipping", [x]) as name:
+        y = py_func(np_d_clipping_32,  # forward pass function
                     [x],
                     [tf.float32],
                     name=name,
@@ -93,24 +90,25 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
 def clipping_grad(op, grad):
     # print("--------In clipping_grad-----------\n")
     # print("op: ", op.inputs[0])
+    # exit(0)
     x = op.inputs[0]
-    n_gr = tf_d_clipping_acti_func(x)  # defining the gradient
+    n_gr = tf_d_clipping(x)  # defining the gradient
     # print("grad: ", grad * n_gr)
     # print("---------------------------------\n")
     return grad * n_gr
 
 #############################################################################
 
-### Combinaing it all together ###
+### Combining it all together ###
 
 
-def np_clipping_acti_func_32(x):
-    return np_clipping_acti_func(x).astype(np.float32)
+def np_clipping_32(x):
+    return np_clipping(x).astype(np.float32)
 
 
-def tf_clipping_acti_func(x, name=None):
-    with tf.compat.v1.name_scope(name, "clipping_acti_func", [x]) as name:
-        y = py_func(np_clipping_acti_func_32,  # forward pass function
+def tf_clipping(x, name=None):
+    with tf.compat.v1.name_scope(name, "clipping", [x]) as name:
+        y = py_func(np_clipping_32,  # forward pass function
                     [x],
                     [tf.float32],
                     name=name,
@@ -124,7 +122,7 @@ def tf_clipping_acti_func(x, name=None):
 if __name__ == "__main__":
     x = tf.compat.v1.placeholder(tf.float32, shape=(4))
     # x = tf.constant([1., 2., 4., 8.], dtype=tf.float32)
-    y = tf_clipping_acti_func(x)
+    y = tf_clipping(x)
     # tf.compat.v1.initialize_all_variables().run()
     # y = -3 * x
     var_grad = tf.gradients(y, x)
